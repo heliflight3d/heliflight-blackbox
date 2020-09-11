c2d10859604da5990ca239caec04d1e33b747353
Petri Mattila <petri.mattila@unipart.io>
2020-09-11 13:52:37 +0100

    HF3D: show raw integer value if format unknown

--- js/flightlog_fields_presenter.js
+++ js/flightlog_fields_presenter.js
@@ -616,7 +616,7 @@ function FlightLogFieldPresenter() {
             	return FlightLogFieldPresenter.decodeDebugFieldToFriendly(flightLog, fieldName, value, currentFlightMode);
 
             default:
-                return "";
+                return (value).toFixed(0);
         }
     };
     
